bb2881ba5ef4c4d5b5a4675051e6e1c308e46742
Sagittarius <ryaboyvlad1712@mail.ru>
2022-04-08 21:30:13 +0700

    Project. Added splash screen, but it will be changed

---
+++ app/src/main/java/com/example/coursproject/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.coursproject;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
